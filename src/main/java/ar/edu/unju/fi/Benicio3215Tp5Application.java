545555f9a269048b03cd76941c08a748783cf8b2
Pc <homecuenta8gmail.com>
2020-06-01 03:00:08 -0300

    Asunto: Inicializacion del proyecto Responsable: Roxana Task00: inicializacion del proyecto Consigna: punto 2 del tp5 gitHub

---
+++ src/main/java/ar/edu/unju/fi/Benicio3215Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Benicio3215Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Benicio3215Tp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/Benicio3215Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Benicio3215Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
